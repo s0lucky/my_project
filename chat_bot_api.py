3fa4d0ffd073c2cb576a1a909be11bd63edf44dc
s0lucky <75899868+s0lucky@users.noreply.github.com>
2021-01-20 23:15:57 +0300

    Update chat_bot_api.py

--- chat_bot_api.py
+++ chat_bot_api.py
@@ -5,24 +5,24 @@ from python123 import token_of_group, token_of_service
 
 vk = vk_api.VkApi(token=token_of_group)
 vk2 = vk_api.VkApi(token=token_of_service)
-owner_id = -200707570
 while True:
-
     info_about_messages = vk.method(
         "messages.getConversations", {"offset": 0, "count": 20, "filter": "unanswered"}
     )
 
     if info_about_messages["count"] >= 1:
         text_of_last_message = info_about_messages["items"][0]["last_message"]["text"]
+        sent = text_of_last_message.split(".")[0]
         user_id = info_about_messages["items"][0]["last_message"]["from_id"]
 
+        if sent == "https://vk":
+            domain = text_of_last_message.split("/")[-1]
+
         info_about_wall = vk2.method(
-            "wall.get", {"offset": 0, "count": 1, "owner_id": owner_id}
+            "wall.get", {"offset": 0, "count": 1, "domain": domain}
         )
-        # ответ
 
         if text_of_last_message.lower() == "привет":
-            # привет
             vk.method(
                 "messages.send",
                 {
@@ -32,96 +32,55 @@ while True:
                 },
             )
 
-        elif text_of_last_message.lower() == "что ты умеешь?":
+        elif text_of_last_message.lower() == "проанализировать группу":
 
             vk.method(
                 "messages.send",
                 {
                     "user_id": user_id,
-                    "message": "Я могу выполнить следующие команды:"
-                               " "
-                               "1)картинка"
-                               " "
-                               "2)проанализировать группу",
+                    "message": "Что вы хотите получить?"
+                               "1)Пост с максимальным количеством лайков"
+                               "2)Пост с максимальным количеством комментариев"
+                               "3)Пост с максимальным количеством репостов",
                     "random_id": random.randint(1, 1000),
                 },
             )
-            if info_about_messages["count"] >= 1:
-                if text_of_last_message.lower() == "картинка":
-                    uploader = vk_api.upload.VkUpload(vk)
-                    image = uploader.photo_messages("4.jpg")
-                    owner_id = str(image[0]["owner_id"])
-                    media_id = str(image[0]["id"])
 
-                    vk.method(
-                        "messages.send",
-                        {
-                            "user_id": user_id,
-                            "attachment": "photo" + owner_id + "_" + media_id,
-                            "random_id": random.randint(1, 1000),
-                        },
-                    )
+        elif text_of_last_message.lower() == "1":
+            count_of_likes = str(info_about_wall["items"][0]["likes"]["count"])
 
-                elif text_of_last_message.lower() == "проанализировать группу":
-                    vk.method(
-                        "messages.send",
-                        {
-                            "user_id": user_id,
-                            "message": "Введите ссылку на группу",
-                            "random_id": random.randint(1, 1000),
-                        },
-                    )
-                    text_of_last_message = info_about_messages["items"][0]["last_message"]["text"] # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                    text_of_last_message.split("/")[-1]# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                    sent = text_of_last_message# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                    if text_of_last_message:
-                        vk.method(
-                            "messages.send",
-                            {
-                                "user_id": user_id,
-                                "message": "Что вы хотите получить?"
-                                           "1)Пост с максимальным количеством лайков"
-                                           "2)Пост с максимальным количеством комментариев"
-                                           "3)Пост с максимальным количеством репостов",
-                                "random_id": random.randint(1, 1000),
-                            },
-                        )
+            vk.method(
+                "messages.send",
+                {
+                    "user_id": user_id,
+                    "message": count_of_likes + "👍🏻",
+                    "random_id": random.randint(1, 1000),
+                },
+            )
 
-                        if text_of_last_message.lower() == "1":
-                            count_of_likes = str(info_about_wall["items"][0]["likes"]["count"])
+        elif text_of_last_message.lower() == "2":
+            count_of_comments = info_about_wall["items"][0]["comments"]["count"]
 
-                            vk.method(
-                                "messages.send",
-                                {
-                                    "user_id": user_id,
-                                    "message": count_of_likes + "👍🏻",
-                                    "random_id": random.randint(1, 1000),
-                                },
-                            )
+            vk.method(
+                "messages.send",
+                {
+                    "user_id": user_id,
+                    "message": count_of_comments,
+                    "random_id": random.randint(1, 1000),
+                },
+            )
 
-                        elif text_of_last_message.lower() == "2":
-                            count_of_comments = info_about_wall["items"][0]["comments"]["count"]
+        elif text_of_last_message.lower() == "3":
+            count_of_reposts = info_about_wall["items"][0]["reposts"]["count"]
 
-                            vk.method(
-                                "messages.send",
-                                {
-                                    "user_id": user_id,
-                                    "message": count_of_comments,
-                                    "random_id": random.randint(1, 1000),
-                                },
-                            )
-
-                        elif text_of_last_message.lower() == "3":
-                            count_of_reposts = info_about_wall["items"][0]["reposts"]["count"]
-
-                            vk.method(
-                                "messages.send",
-                                {
-                                    "user_id": user_id,
-                                    "message": count_of_reposts,
-                                    "random_id": random.randint(1, 1000),
-                                },
-                            )
+            vk.method(
+                "messages.send",
+                {
+                    "user_id": user_id,
+                    "message": count_of_reposts,
+                    "random_id": random.randint(1, 1000),
+                },
+            )
 
         else:
             vk.method(
